8311b63bedeaf265463c30888d4e595ba18e53a2
GiovanaCardosoTech <124629718+GiovanaCardosoTech@users.noreply.github.com>
2024-09-23 00:46:47 -0300

    Update RPA_Test.py

--- RPA_Test.py
+++ RPA_Test.py
@@ -5,6 +5,10 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 import time
 
+data = '09-23-2024'
+hora = '19:08:23'
+cotacao_dolar = 5.44
+
 try:
     connection = psycopg2.connect(user="avnadmin",
                                     password="AVNS_I2YSg3hf2GFrMM8X7eZ",
@@ -13,7 +17,7 @@ try:
                                     database="dbCotacoes")
     cursor = connection.cursor()
 
-    cursor.execute('CALL inserir_cotacao_dolar('09-23-2024', '19:08:23', 5.44)', (data, hora, cotacao_dolar))
+    cursor.execute('CALL inserir_cotacao_dolar(%s, %s, %s)', (data, hora, cotacao_dolar))
     connection.commit()
     print("Dados inseridos com sucesso!")
 
